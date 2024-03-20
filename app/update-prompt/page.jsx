00d1f5e7099bee0d15b116e115ecb011a18ff6d5
sameerparmar-kryoverse <sameer@kryoverse.com>
2024-03-20 10:24:18 +0530

    fix: wrap update-prompt in suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { Suspense, useEffect, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import Form from "@/components/Form"
 
@@ -55,13 +55,16 @@ const EditPrompt = () => {
     }
 
   return (
-    <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={editPrompt}
-    />
+    <Suspense>
+        <Form
+            type="Edit"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={editPrompt}
+        />
+    </Suspense>
+    
   )
 }
 
